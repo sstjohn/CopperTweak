e5afea3eaf3d7f107b40bfd61800d08ff6b5c833
Saul St. John <saul@betafive.net>
2012-12-05 15:11:53 -0600

    variable number of tone maps graphed

--- plot.py
+++ plot.py
@@ -4,32 +4,29 @@ from time import sleep
 
 PLOT_TIME = 120
 
+class ToneMap():
+	def __init__(self, index, x):
+		self.x = x
+		self.y = []
+		self.index = index
+		self.line, = pylab.plot(self.x, self.y)
+
+	def update(self):
+		try:
+			self.y.append(hpav.get_def_tmi_score(self.index))
+		except:
+			self.y.append(0)
+		self.line.set_xdata(self.x)
+		self.line.set_ydata(self.y)
+
 if __name__ == "__main__":
-	x = [0]
-	y0 = [hpav.get_def_tmi_score(0)]
-	y1 = [hpav.get_def_tmi_score(1)]
-	y2 = [hpav.get_def_tmi_score(2)]
-	y3 = [hpav.get_def_tmi_score(3)]
-	ylim = y0[0] * 3
+	x = []
 	pylab.ion()
-	line0, = pylab.plot(x, y0, '-')
-	line1, = pylab.plot(x, y1, 'r-')
-	line2, = pylab.plot(x, y2, 'b-')
-	line3, = pylab.plot(x, y3, 'g-')
-	pylab.axis([0, PLOT_TIME, 0, ylim])
-        for i in range(1, PLOT_TIME * 5):
+	pylab.axis([0, PLOT_TIME, 0, 4096])
+	tms = [ToneMap(i, x) for i in range(0,10)]
+        for i in range(0, PLOT_TIME * 5):
 		x.append(i / 5)
-		y0.append(hpav.get_def_tmi_score(0))
-		y1.append(hpav.get_def_tmi_score(1))
-		y2.append(hpav.get_def_tmi_score(2))
-		y3.append(hpav.get_def_tmi_score(3))
-		line0.set_xdata(x)
-		line0.set_ydata(y0)
-		line1.set_xdata(x)
-		line1.set_ydata(y1)
-		line2.set_xdata(x)
-		line2.set_ydata(y2)
-		line3.set_xdata(x)
-		line3.set_ydata(y3)
+		for tm in tms:
+			tm.update()
 		pylab.draw()
 		sleep(1/5)
