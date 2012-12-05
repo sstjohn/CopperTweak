722d312a21449ca5777e094bc014d45423e77554
Saul St. John <sstjohn@cs.wisc.edu>
2012-12-05 12:02:50 -0600

    cheap plot hacking

--- plot.py
+++ plot.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 from time import sleep
 
 if __name__ == "__main__":
-	X = []
-	Y = []
+	x = []
+	y = []
+	line, = plt.plot(x, y, '-')
         for i in range(0, 120):
-		X.append(i)
-		Y.append(i ** 2)
-	plt.plot(X, Y, "-")
-	plt.show()
+		x.append(i)
+		y.append(i ** 2)
+		line.set_xdata(x)
+		line.set_ydata(y)
+		plt.draw()
+		sleep(1)
